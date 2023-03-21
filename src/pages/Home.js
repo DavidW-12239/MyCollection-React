1a10a397d4d0ed8cda47dde2f2835c134de5ba2c
DavidWang <davidw12239@gmail.com>
2023-03-21 11:04:22 -0700

    second

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,19 @@ import { PORT } from '../config';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-
+import AddCollectionForm from '../utils/AddCollectionForm'
+import UpdateCollectionForm from "../utils/UpdateCollectionForm";
+import UpdateCollectionFile from "../utils/UpdateCollectionFile";
 
 function Home() {
+  const navigate = useNavigate();
+  const { userId } = useParams();
   const [user, setUser] = useState({});
   const [collections, setCollections] = useState([]);
-  const { userId } = useParams();
-  const navigate = useNavigate();
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [showUpdateFile, setShowUpdateFile] = useState(false);
+  const [selectedCollection, setSelectedCollection] = useState(null);
 
   const fetchUser = () => {
     fetch(`http://localhost:${PORT}/user/${userId}/profile`)
@@ -39,23 +45,142 @@ function Home() {
     fetchCollections();
   },[]);
 
-  const handleClick = (collectionId) => {
+  const handleAddCollection = (formData) => {
+    const requestOptions = {
+      method: 'POST',
+      body: formData
+    };
+  
+    fetch(`http://localhost:${PORT}/collection/${userId}/addMainCollection`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        setCollections((prevCollections) => [...prevCollections, data]);
+        setShowAddForm(false);
+        fetchCollections();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  const handleCloseAddForm = () =>{
+    setShowAddForm(false);
+  }
+
+  const handleSubCollections = (collectionId) => {
     navigate(`/${userId}/collection/${collectionId}/subCollections`);
+  };
+
+  const handleShowUpdateForm = (collection) => {
+    setSelectedCollection(collection);
+    setShowUpdateForm(true);
+  };
+
+  const handleCloseUpdateForm = () => {
+    setShowUpdateForm(false);
+  };
+
+  const handleShowUpdateFile = (collection) => {
+    setSelectedCollection(collection);
+    setShowUpdateFile(true);
+  };
+
+  const handleCloseUpdateFile = () => {
+    setShowUpdateFile(false);
+  };
+
+  const handleUpdateCollectionContext = (collectionId, formData) => {
+    const requestOptions = {
+      method: 'POST',
+      body: formData,
+    };
+
+    fetch(`http://localhost:${PORT}/collection/${collectionId}/updateCollectionContext`, requestOptions)
+      .then((response) => response.json())
+      .then((updatedCollection) => {
+        setCollections((prevCollections) =>
+        prevCollections.map((collection) =>
+          collection.collectionId === collectionId ? updatedCollection : collection
+        )
+      );
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  const handleUpdateCollectionImage = (collectionId, formData) => {
+    const requestOptions = {
+      method: 'POST',
+      body: formData,
+    };
+
+    fetch(`http://localhost:${PORT}/collection/${collectionId}/updateCollectionImage`, requestOptions)
+      .then((response) => response.json())
+      .then((updatedCollection) => {
+        setCollections((prevCollections) =>
+        prevCollections.map((collection) =>
+          collection.collectionId === collectionId ? updatedCollection : collection
+        )
+      );
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  const handleDeleteMainCollection  = (collectionId) => {
+    fetch(`http://localhost:${PORT}/collection/${userId}/${collectionId}/deleteMainCollection`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        setCollections(collections.filter(collection => collection.collectionId !== collectionId))
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
   
   return (
     <div className="container">
       <h1>Welcome, {user.userName}</h1>
+
+      <button onClick={() => setShowAddForm(!showAddForm)}>Add Collection</button>
+      {showAddForm && 
+      <AddCollectionForm 
+        addCollection={handleAddCollection} 
+        onClose={handleCloseAddForm}
+      />}
+
+      {showUpdateForm && selectedCollection && (
+        <UpdateCollectionForm
+          collection={selectedCollection}
+          onUpdate={handleUpdateCollectionContext}
+          onClose={handleCloseUpdateForm}
+        />
+      )}
+
+      {showUpdateFile && selectedCollection && (
+        <UpdateCollectionFile
+          collection={selectedCollection}
+          onUpdate={handleUpdateCollectionImage}
+          onClose={handleCloseUpdateFile}
+        />
+      )}
+
       <div>
         {collections.map((collection) => (
-          <Card key={collection.collectionId}
-          title={collection.title}
-          website={collection.websiteAddress}
-          image={`${process.env.PUBLIC_URL}/images/${collection.imagePath}`}
-          context={collection.description}
-          buttonText="Click me"
-          onClick={() => handleClick(collection.collectionId)}
-          />
+          <Card
+            key={collection.collectionId}
+            collection={collection}
+            image={`${process.env.PUBLIC_URL}/images/${collection.image}`}
+            onClick={() => handleSubCollections(collection.collectionId)}
+            onShowUpdateFile={() => handleShowUpdateFile(collection)}
+            onShowUpdateForm={() => handleShowUpdateForm(collection)}
+            onDelete={() => handleDeleteMainCollection(collection.collectionId)}
+        />
         ))}
       </div>
     </div>
