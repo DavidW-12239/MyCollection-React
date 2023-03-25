67aca1aeeec16264c48fea2d29b545404f9a5eef
DavidWang <davidw12239@gmail.com>
2023-03-24 18:39:09 -0700

    friday commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import SubCollections from './pages/SubCollections'
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />}/>
         <Route path="/home/:userId" element={<Home />} />
         <Route path="/:userId/collection/:collectionId/subCollections" element={<SubCollections />} />
       </Routes>
